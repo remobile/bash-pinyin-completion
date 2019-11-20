688c4219217465a992c8e54453c858f38511af64
fov42550564 <42550564@qq.com>
2019-11-20 22:36:21 +0800

    xx

--- README.md
+++ README.md
@@ -5,42 +5,32 @@
   * 仅支持UTF-8编码环境
 
 ## 安装
-### ubuntu
-支持10.04 ~ 12.10版本从ppa安装
+### mac
+* 首先需要安装 bash_complete
 
-    sudo add-apt-repository ppa:emptyhua/toolbox
-    sudo apt-get update
-    sudo apt-get install bash-pinyin-completion
+``` sh
+brew install bash_complete
+```
+* 然后进入工程，编译，安装
 
-然后新开一个终端，就可以使用拼音补齐了
+``` sh
+make
+make install
+```
 
-### Other Linux
-对于其他发行版本，只要安装了bash－completion都可以使用源码安装(已测的bash－completion最低版本为1.1)
+## 规则
 
-    git clone git://github.com/emptyhua/bash-pinyin-completion.git
-    cd bash-pinyin*
-    make
-    sudo make install
-    
-然后新开一个终端，就可以使用拼音补齐了
+```
+ 实验目录如下：
+ ls
+ SVN培训  全球眼  浙江建行  浙江农信
 
-### Mac OS X
-对于mac需要先安装 bash-completion
-
-    sudo port install bash-completion
-
-然后编辑~/.bash_profile,把下面的代码贴进去
-
-    if [ -f /opt/local/etc/bash_completion ]; then
-    . /opt/local/etc/bash_completion
-    fi
-
-下载源码,并编译,编译之前根据自己的xcode安装目录修改Makefile中的XCODE_SDK变量
-
-    git clone git://github.com/emptyhua/bash-pinyin-completion.git
-    cd bash-pinyin*
-    make
-    sudo make install
-
-然后新开一个终端，就可以使用拼音补齐了
-
+ 使用: (输完后按 TAB 键自动补全)
+    cd S     <tab>             进入[SVN培训]
+    cd q     <tab>             进入[全球眼]
+    cd z     <tab>             自动补全[浙江]
+    cd zj    <tab><tab>        提示[浙江建行 浙江农信]备选
+    cd zjj   <tab>             进入[浙江建行]
+    cd zj1   <tab>             进入[浙江建行]
+    cd zj2   <tab>             进入[浙江农信]
+```
--- pinyinmatch.c
+++ pinyinmatch.c
@@ -1,139 +1,118 @@
+/*
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * Author : emptyhua@gmail.com
+ * Create : 2011.9.26
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
 #include <getopt.h>
-#include <string.h>
 #include "pinyin.h"
 #include "utf8vector.h"
 #include "linereader.h"
 
-#define true 1
-#define false 0
-typedef int bool;
+// #define DEBUG
+#ifdef DEBUG
+#define MYLOG(x, ARGS...) fprintf (stderr, "\n%d:"x"\n", __LINE__, ##ARGS)
+#else
+#define MYLOG(x, ARGS...)
+#endif //DEBUG
 
-int index_of(const char* str, int offset, char ch) {
-    int index = 0;
-    while(str[index] != '\0'){
-        if(index > offset && str[index] == ch){
-            return index;
-        }
-        index++;
-    }
-    return -1;
-}
-bool match_item(char* parent, const char *child) {
-    if (parent[0] != child[0]) {
-        return false;
-    }
-    int last_index = -1;
-    int count = 0;
-    int child_len = strlen(child);
-    for (int i = 0; i < child_len; i++) {
-        char ch = child[i];
-        int index = index_of(parent, last_index, ch);
-        if (index > last_index) {
-            count++;
-        }
-        last_index = index;
-    }
-    return count == child_len;
-}
-bool match(char** parents, int parent_count, const char *child) {
-    for (int i = 0; i < parent_count; i++) {
-        if (match_item(parents[i], child)) {
-            return true;
-        }
-    }
-    return false;
-}
-void set_parents_pinyins(char **parents, int parent_count, const char **pinyins, int *count_list, int level) {
-    int pre_count = 1;
-    for (int i = 0; i < level; i++) {
-        pre_count *= count_list[i];
-    }
-    int current_count = count_list[level];
-    for (int i = 0; i < parent_count; i++) {
-        strcat(parents[i], pinyins[(i/pre_count)%current_count]);
-    }
-}
-void set_parents_char(char **parents, int parent_count, char ch) {
-    for (int i = 0; i < parent_count; i++) {
-        parents[i][strlen(parents[i])] = ch;
-    }
-}
-bool match_line(const char *line, int line_length, const char *word) {
-    wchar_t line_char;
+
+int match_line_with_keyword(const char *line, int line_length, const char *keyword) {
+    // MYLOG("line=%s,keyword=%s", line, keyword);
+
+    wchar_t line_char, keyword_char;
+    int match_hanzi_count = 0;
+
     utf8vector line_vector = utf8vector_create(line, line_length);
-    int size = utf8vector_uni_count(line_vector);
+    utf8vector keyword_vector = utf8vector_create(keyword, -1);
 
-    int parent_count = 1;
-    int *count_list = (int *)calloc(size, sizeof(int));
-    int level = 0;
-    while((line_char = utf8vector_next_unichar(line_vector)) != '\0') {
+    int keyword_length = utf8vector_uni_count(keyword_vector);
+    wchar_t *keyword_uni = malloc(sizeof(wchar_t) * keyword_length);
+
+    int keyword_index = 0;
+    while ((keyword_char = utf8vector_next_unichar(keyword_vector)) != '\0') {
+        keyword_uni[keyword_index] = keyword_char;
+        keyword_index ++;
+    }
+
+    int match_rt = 1;
+    keyword_index = 0;
+
+    while((line_char = utf8vector_next_unichar(line_vector)) != '\0' && keyword_index < keyword_length) {
+        keyword_char = keyword_uni[keyword_index];
         if (pinyin_ishanzi(line_char)) {
-            int count = get_pinyin_count(line_char);
-            parent_count *= count;
-            count_list[level] = count;
-            level++;
-        }
-    }
-    utf8vector_reset(line_vector);
-    char **parents = (char **)calloc(parent_count, sizeof(char *));
-    for (int i = 0; i < parent_count; i++) {
-        parents[i] = (char *)calloc(size * 6 + 1, sizeof(char));
-    }
-
-    level = 0;
-    while((line_char = utf8vector_next_unichar(line_vector)) != '\0') {
-        if (pinyin_ishanzi(line_char)) {
-            const char **pinyins;
-            pinyin_get_pinyins_by_unicode(line_char, &pinyins);
-            set_parents_pinyins(parents, parent_count, pinyins, count_list, level);
-            level++;
-            free(pinyins);
-        } else {
-            set_parents_char(parents, parent_count, line_char);
-        }
-    }
-    bool result = match(parents, parent_count, word);
-    utf8vector_free(line_vector);
-    for (int i = 0; i < parent_count; i++) {
-        free(parents[i]);
-    }
-    free(parents);
-    free(count_list);
-
-    return result;
-}
-int main(int argc, char **argv) {
-    char *word = argv[1];
-    int word_len = strlen(word);
-    int select = 0;
-    if (word_len > 2 && word[word_len-2] == '@') { // fyj@1的格式为查看第几个
-        if (word[word_len-1] > 48 && word[word_len-1] < 58) {
-            select = word[word_len-1] - 48;
-            word[word_len-2] = '\0';
-        }
-    }
-
-    int count;
-    int show_index = 0;
-    linereader reader = linereader_create(STDIN_FILENO);
-    while ((count = linereader_readline(reader)) != -1) {
-        const char *line = reader->line_buffer;
-        if (word_len == 0 || match_line(line, count, word)) {
-            if (select == 0) {
-                printf("%s\n", line);
-            } else {
-                show_index++;
-                if (show_index == select) {
-                    printf("%s\n", line);
+            if (pinyin_ishanzi(keyword_char)) {
+                if (line_char != keyword_char) {
+                    match_rt = 0;
                     break;
                 }
+            } else if (pinyin_isabc(keyword_char)) {
+                keyword_char = pinyin_lowercase(keyword_char);
+                const char **pinyins;
+                int count = pinyin_get_pinyins_by_unicode(line_char, &pinyins);
+                int finded = 0;
+                for (int i = 0; i < count; i++) {
+                    if (keyword_char == pinyins[i][0]) {
+                        finded = 1;
+                        break;
+                    }
+                }
+                free(pinyins);
+
+                if (finded == 0) {
+                    match_rt = 0;
+                } else {
+                    match_hanzi_count ++;
+                }
+                if (match_rt == 0) {
+                    break;
+                }
+            } else {
+                //printf("not hanzi or abc %d\n", keyword_char);
+                match_rt = 0;
+                break;
             }
+        } else {
+            if (line_char != keyword_char) {
+                match_rt = 0;
+                break;
+            }
+        }
+        keyword_index ++;
+    }
+
+    if (match_rt == 1 && keyword_index < keyword_length) {
+        match_rt = 0;
+    }
+
+    free(keyword_uni);
+    utf8vector_free(line_vector);
+    utf8vector_free(keyword_vector);
+    if (match_rt == 0) {
+        return -1;
+    } else {
+        return match_hanzi_count;
+    }
+}
+
+int main(int argc, char **argv) {
+    char *keyword = argv[1];
+    linereader reader = linereader_create(STDIN_FILENO);
+    int count;
+    while ((count = linereader_readline(reader)) != -1)
+    {
+        const char *line = reader->line_buffer;
+        int match_count = match_line_with_keyword(line, count, keyword);
+        MYLOG("match_count=%d", match_count);
+        if (match_count > 0) {
+            printf("%.*s\n", count, line);
         }
     }
     linereader_free(reader);
-    return false;
+    return 0;
 }
